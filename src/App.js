4191409c346e14a06d4d621bfa9012abbb7259c4
Matebul <mohammadmatebul047890@gmail.com>
2022-10-04 16:04:42 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+ import Resturant from "./component/Basics/Resturant"; 
+ //import UseEffect from "./component/Hooks/useEffect";
+ //import UseState from "./component/Hooks/useState";
+ //import UseReducer from "./component/Hooks/useReducer";
+
+const App = () => {
+  return <Resturant/>
+};
+
+export default App;
